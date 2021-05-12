8ecbec9a850bdb93c28ba7819aa3ffbb2e80b1bd
Hakimcherfi <hakimcherfi31@gmail.com>
2021-05-12 17:39:26 +0200

    Ecriture forwardpass, bugfix, a suivre : boucle convergence

--- ddp.py
+++ ddp.py
@@ -3,24 +3,21 @@ import numpy as np
 import matplotlib.pylab as plt
 from numpy import linalg
 
-T = 2 #T+1 points
+T = 10 #T+1 points
 m = 1.
-s0 = np.array([[1.],[1.]])
+s0 = np.array([[10.],[-3.]])
 starg = np.array([[0.],[0.]])
 sweight = 1.
 uweight = 1.
 sweightT = 1.
 
 u = np.zeros((2,T)) #commande en vitesse : vx,vy ; nu * T
-#u[0:1,:]=1
-#u[1:2,:]=1
 Cs = sweight*np.eye(s0.shape[0])
 Cu = uweight*np.eye(u.shape[0])
 CsT = sweightT*np.eye(s0.shape[0])
-
 dt = 1/T
-sumcosts = 0.
 s=np.tile(s0,(1,T+1)) #ns * T+1 #attention :  dtype float...
+
 assert(s.shape==(2,T+1))
 assert(u.shape==(2,T))
 
@@ -39,32 +36,28 @@ def grad_finalcost(s):
 def hess_finalcost():
     return CsT
 
-for t in range(0,T):
-    st = s[:,t:t+1]
-    ut = u[:,t:t+1]
-    sumcosts += cost(st,ut)
-    s[:,t+1:t+2] = next_state(st,ut)
-sumcosts += finalcost(s[:,T:T+1])
-
-print("cout :")
-print(sumcosts)
-print("s=")
-print(s)
-print("u=")
-print(u)
-plt.scatter(s[0:1,:],s[1:2,:])
+def calcul_cout_et_affichage(s,u):
+    sumcosts = 0.
+    for t in range(0,T):
+        st = s[:,t:t+1]
+        ut = u[:,t:t+1]
+        sumcosts += cost(st,ut)
+        s[:,t+1:t+2] = next_state(st,ut)
+    sumcosts += finalcost(s[:,T:T+1])
+    print("cout :")
+    print(sumcosts)
+    print("s=")
+    print(s)
+    print("u=")
+    print(u)
+    plt.scatter(s[0:1,:],s[1:2,:])
 
 ######
-
-
-
-
-
 
 Fx = np.eye(2)
 Fu = dt*np.eye(2)
 
-def backwardpass():  
+def backwardpass(s):  
     Vx = [] #termes d'ordre 1 de T a 0
     Vxx = [] #termes d'ordre 2 de T a 0
     k = [] #gains de T a 0
@@ -76,27 +69,27 @@ def backwardpass():
 
     for t in range(T-1,-1,-1): #T-1 a 0
         #at time t < T
-        Qx = Vx[-1].T@Fx + Cs@s[:,t:t+1]
-        Qu = Vx[-1].T@Fu + Cu@u[:,t:t+1]
-        Qxx = Fx.T@Vxx[0]@Fx + Cs
-        Quu = Fu.T@Vxx[0]@Fu + Cu
-        Qxu = Fx.T@Vxx[0]@Fu #+Lxu...
-        Qux = Fu.T@Vxx[0]@Fx #+Lux...
-        k.append(-np.linalg.inv(Quu)@Qu)
-        K.append(-np.linalg.inv(Quu)@Qux)
+        Qx = Cs@s[:,t:t+1] + Fx.T@Vx[-1]
+        Qu = Cu@u[:,t:t+1] + Fu.T@Vx[-1]
+        Qxx = Cs + Fx.T@Vxx[-1]@Fx 
+        Quu = Cu + Fu.T@Vxx[-1]@Fu
+        Qxu = Fx.T@Vxx[-1]@Fu #+Lxu...
+        Qux = Fu.T@Vxx[-1]@Fx #+Lux...
+        k.append(np.linalg.inv(Quu)@Qu)
+        K.append(np.linalg.inv(Quu)@Qux)
         Vx.append(Qx-Qxu@k[-1])
         Vxx.append(Qxx-Qxu@K[-1])
+    return k,K
 
-def forwardpass():
-    
+def forwardpass(k,K,u,s):
+    for t in range (T):
+        u[:,t:t+1]=-k[-1]-K[-1]@s[:,t:t+1]
+        k.pop()
+        K.pop()
+        s[:,t+1:t+2] = next_state(s[:,t:t+1],u[:,t:t+1])
+    return s,u
 
-def hess(Lxx,Luu,Lxu,Lux):
-    Lxx = Cs
-    Luu = Cu
-    Lxu = np.zeros((s0.shape[0],u.shape[0]))
-    Lux = np.zeros((u.shape[0],s0.shape[0]))
-    Hess1 = np.concatenate((Lxx,Lxu),axis=1)
-    Hess2 = np.concatenate((Lux,Luu),axis=1)
-    return np.concatenate((Hess1,Hess2),axis=0)
+k,K = backwardpass(s)
+s,u = forwardpass(k,K,u,s)
 
-backwardpass()
+calcul_cout_et_affichage(s,u)
