db5d4f5197e9ea6db50fef33be0ea908828da652
Hakimcherfi <hakimcherfi31@gmail.com>
2021-05-17 14:26:45 +0200

    Correction bugs forwardpass, ajout fonction affichage 1D et assertions

--- ddp.py
+++ ddp.py
@@ -3,24 +3,28 @@ import numpy as np
 import matplotlib.pylab as plt
 from numpy import linalg
 
-
-T = 1000
-N = 1000 #N+1 points
-x0 = np.array([[2.],[2.]]) #contrainte
-xtarg = np.array([[0.],[0.]])
+T = 10
+N = 10 #N+1 points
+x0 = np.array([[-2.],[-2.]]) #contrainte
+xtarg = np.array([[3.],[1.]])
+dimx = 2
+dimu = 2
 xweight = 1.
 uweight = 1.
 xweightT = 1.
 
-u = np.zeros((2,N)) #commande en vitesse : vx,vy ; nu * N
-Cx = xweight*np.eye(x0.shape[0])
-Cu = uweight*np.eye(u.shape[0])
-CxT = xweightT*np.eye(x0.shape[0])
+#####
+
+assert((dimx,1)==x0.shape)
+assert((dimx,1)==xtarg.shape)
+u = np.zeros((dimu,N)) #commande en vitesse : vx,vy ; nu * N
+Cx = xweight*np.eye(dimx)
+Cu = uweight*np.eye(dimu)
+CxT = xweightT*np.eye(dimx)
 dt = T/N
 x=np.tile(x0,(1,N+1)) #nx * N+1 #attention :  dtype float...
-
-assert(x.shape==(2,N+1))
-assert(u.shape==(2,N))
+Fx = np.eye(dimx)
+Fu = dt*np.eye(dimu)
 
 def next_state(x,u):
     return x + u*dt
@@ -49,26 +53,7 @@ def LxT(x):
 def LxxT():
     return CxT
 
-def calcul_cout_et_affichage(x,u):
-    sumcosts = 0.
-    for t in range(0,N):
-        xt = x[:,t:t+1]
-        ut = u[:,t:t+1]
-        sumcosts += cost(xt,ut)
-        x[:,t+1:t+2] = next_state(xt,ut)
-    sumcosts += finalcost(x[:,N:N+1])
-    print("cout :")
-    print(sumcosts)
-    print("x=")
-    print(x)
-    print("u=")
-    print(u)
-    plt.scatter(x[0:1,:],x[1:2,:])
-
-Fx = np.eye(2)
-Fu = dt*np.eye(2)
-
-def backwardpass(x):
+def backwardpass(x,u):
     Vx = [] #termes d'ordre 1 de N a 0
     Vxx = [] #termes d'ordre 2 de N a 0
     k = [] #gains de N a 0
@@ -92,14 +77,34 @@ def backwardpass(x):
         Vxx.append(Qxx-Qxu@K[-1])
     return k,K
 
-def forwardpass(k,K,x):
+def forwardpass(k,K,x,u):
+    dx = np.zeros((dimx,N+1))
+    du = np.zeros((dimu,N))
     for t in range (N):
-        u[:,t:t+1]=-k[-1]-K[-1]@x[:,t:t+1]
+        du[:,t:t+1]=-k[-1]-K[-1]@dx[:,t:t+1]
+        dx[:,t+1:t+2] = next_state(x[:,t:t+1]+dx[:,t:t+1],u[:,t:t+1]+du[:,t:t+1])-x[:,t+1:t+2]
         k.pop()
         K.pop()
-        x[:,t+1:t+2] = next_state(x[:,t:t+1],u[:,t:t+1])
+    x+=dx
+    u+=du
     return x,u
 
-k,K = backwardpass(x)
-x,u = forwardpass(k,K,x)
+def calcul_cout_et_affichage(x,u):
+    sumcosts = 0.
+    for t in range(0,N):
+        sumcosts += cost(x[:,t:t+1],u[:,t:t+1])
+    sumcosts += finalcost(x[:,N:N+1])
+    print("cout :")
+    print(sumcosts)
+    print("x=")
+    print(x)
+    print("u=")
+    print(u)
+    if (dimx==2):
+        plt.scatter(x[0:1,:],x[1:2,:])
+    if(dimx==1):
+        plt.scatter(x[0:1,:],np.zeros((1,x[0:1,:].shape[1])))
+
+k,K = backwardpass(x,u)
+x,u = forwardpass(k,K,x,u)
 calcul_cout_et_affichage(x,u)
